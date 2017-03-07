16c62a2913e61af6cc1f8b867b4aa7a4eebc3625
Ben Gatewood <ben@gatewood.org.uk>
2017-03-07 21:36:45 +0000

    Tidy

--- phoneData.R
+++ phoneData.R
@@ -1,6 +1,7 @@
 library(XML)
 library(plyr)
 library(lubridate)
+library(dplyr)
 
 rawXML <- xmlParse("./apple_health_export/export.xml")
 
@@ -9,4 +10,30 @@ nodes <- getNodeSet(rawXML, "//Record")
 xmlList <- xmlToList(rawXML)
 
 phoneData <- ldply(xmlList[4:length(xmlList) - 1])
+
+filter(phoneData, type == "HKQuantityTypeIdentifierStepCount") %>%
+        mutate(creationDate = ymd_hms(creationDate),
+               startDate = ymd_hms(startDate),
+               endDate = ymd_hms(endDate),
+               value = as.numeric(value),
+               device = NULL,
+               type = "steps") -> steps
+        
+filter(phoneData, type == "HKQuantityTypeIdentifierFlightsClimbed") %>%
+        mutate(creationDate = ymd_hms(creationDate),
+               startDate = ymd_hms(startDate),
+               endDate = ymd_hms(endDate),
+               value = as.numeric(value),
+               device = NULL,
+               type = "num_stairs") -> stairs
+
+filter(phoneData, type == "HKQuantityTypeIdentifierDistanceWalkingRunning") %>%
+        mutate(creationDate = ymd_hms(creationDate),
+               startDate = ymd_hms(startDate),
+               endDate = ymd_hms(endDate),
+               value = as.numeric(value) * 1000,
+               device = NULL,
+               unit = "metres",
+               type = "distance") -> distance
+
 
