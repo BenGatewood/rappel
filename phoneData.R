809c4289e2ab00b3ecce18d88d10f4ced367a685
Ben Gatewood <ben@gatewood.org.uk>
2017-03-07 19:37:42 +0000

    Turns out it was simpler

--- phoneData.R
+++ phoneData.R
@@ -1,30 +1,12 @@
 library(XML)
+library(plyr)
 library(lubridate)
 
 rawXML <- xmlParse("./apple_health_export/export.xml")
 
 nodes <- getNodeSet(rawXML, "//Record")
 
-extractRow <- function(x) {
-        
-        colNames <- c("type", "sourceName", "sourceVersion",
-                   "unit", "creationDate", "startDate",
-                   "endDate", "value")
-        
-        rowList <- list(type <- xmlGetAttr(x, "type"),
-                        sourceName <- xmlGetAttr(x, "sourceName"),
-                        sourceVersion <- xmlGetAttr(x, "sourceVersion"),
-                        unit <- xmlGetAttr(x, "unit"),
-                        creationDate <- xmlGetAttr(x, "creationDate"),
-                        startDate <- xmlGetAttr(x, "startDate"),
-                        endDate <- xmlGetAttr(x, "endDate"),
-                        value <- xmlGetAttr(x, "value"))
-        
-        row <- data.frame(rowList)
-        
-        names(row) <- colNames
-        
-        row
-}
+xmlList <- xmlToList(rawXML)
 
+phoneData <- ldply(xmlList[4:length(xmlList) - 1])
 
